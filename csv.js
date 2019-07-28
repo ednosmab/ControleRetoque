870ee017457cccfc2e314355f34a625825998793
Edson Garcia <ednosmab@yahoo.com.br>
2019-07-27 23:19:00 -0300

    Trabalhando com arquivos CSV, nesse exemplo ha um sistema de validacao e insercao de informacoes armazenadas em aquivo que sera salvo posteriormente na maquina do usuario

---
+++ csv.js
@@ -0,0 +1,21 @@
+﻿function _gerarCsv(){
+    let user = Number(document.querySelector("#ususario").value)
+    let idservico = Number(document.querySelector("#IDServico").value)
+    let motivo = document.querySelector('#motivo').value
+    let date = new Date();
+    let hora = date.getHours() + '.' + date.getMinutes() + '.' + date.getSeconds()
+    let data = date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + hora
+
+    if(user == 0 || idservico == 0 || user == null || idservico == null || motivo == 0 || motivo == null)
+        {alert('Usuário, ID do Serviço, ou Motivo para retoque estão inválidos')}
+    else{
+        csv = 'Data: ' + data + '\nUsuario: ' + user + '\nID: '+ idservico + '\nMotivo: ' + motivo
+        let linkDonw = document.createElement('a')
+        linkDonw.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv)
+        linkDonw.target = '_blank'
+        linkDonw.download = `${idservico}-${hora}.csv`
+        linkDonw.click()
+        window.location.replace("csv.html")
+    }
+
+}
